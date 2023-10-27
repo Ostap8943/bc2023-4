2019b2aeb520c81e1a476157be60b9b5de6111a5
Ostap8943 <ostap5673@gmail.com>
2023-10-27 11:12:02 +0300

    Коміт

--- server.js
+++ server.js
@@ -1,36 +1,51 @@
-const http = require("http");
-const fs = require("fs");
-
-const host = 'localhost';
+const fs = require('fs');
+const fastXmlParser = require('fast-xml-parser');
+const http = require('http');
+ 
+const hostname = 'localhost';
 const port = 8000;
 
-const requestListener = function (req, res) {
-  fs.readFile('data.xml', 'utf-8', (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end("Помилка сервера");
-    } else {
-      const filteredData = filterXMLData(data);
-      res.writeHead(200, { 'Content-Type': 'application/xml' });
-      res.end(filteredData);
-    }
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    fs.readFile('data.xml', 'utf-8', (err, data) => {
+      if (err) {
+        console.error('Error reading the XML file:', err);
+        return;
+         }
+      const parser = new fastXmlParser.XMLParser()
+        try {
+          // Parse the XML
+         const options = {
+            attributeNamePrefix: "@_",
+            attrNodeName: false,
+            textNodeName: "#text",
+            ignoreAttributes: false,
+            ignoreNameSpace: false,
+            parseNodeValue: true,
+            parseAttributeValue: true,
+            trimValues: true,
+            parseTrueNumberOnly: false,
+          };
+      
+        const result = parser.parse(data, options);
+        const builder = new fastXmlParser.XMLBuilder();
+        const filtered = result.indicators.inflation.filter(v => v.ku == 13 && v.value > 5);
+        const values = [];
+        for (const i of filtered) {
+            values.push(i['value']);
+        }
+        const newObj = builder.build({'data': {'value' : values } } );
+        console.log(newObj);
+
+         res.end(newObj);
+        
+        } catch (parseErr) {
+          console.error('Error parsing XML:', parseErr);
+        }
+      });
   });
-};
-
-function filterXMLData(xmlData) {
-  try {
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(xmlData, "text/xml");
-    const xmlResult = new XMLSerializer().serializeToString(xmlDoc);
-
-    return xmlResult;
-  } catch (err) {
-    console.error('Помилка при обробці XML:', err);
-    return xmlData;
-  }
-}
-const server = http.createServer(requestListener);
-
-server.listen(port, host, () => {
-  console.log(`Сервер працює за адресою http://${host}:${port}`);
-});
+ 
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+  });
